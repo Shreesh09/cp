48dae652f31a0c6a6b3b8852e7f2d63d97b57ae6
Shreesh <shreeshnautiyal3@gmail.com>
2024-03-23 11:28:08 +0530

    900th sheet 6th done

---
+++ tle_eliminators/900_6.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i, a, b) for (int i = a; i < b; ++i)
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+typedef long long int ll;
+ 
+void init_code() {
+    #ifndef ONLINE_JUDGE
+        freopen("../in.txt", "r", stdin);
+        freopen("../out.txt", "w", stdout);
+    #endif
+}
+ 
+int main(void) {
+    init_code();
+    int t = 0;
+    cin >> t;
+    while(t--) {
+        ll n;
+        cin >> n;
+        ll count = 0;
+
+        for(ll i = 1; i <= n; i++) {
+            if(n % i != 0)
+                break;
+            count++;
+        }
+
+        cout<<count<<endl;
+
+    }
+    return 0;
+}
