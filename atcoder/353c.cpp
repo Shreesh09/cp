5812c349954b303b97870c855ee8ae39ba2a5b89
Shreesh <shreeshnautiyal3@gmail.com>
2024-05-17 12:17:03 +0530

    upsolved previous contests

--- atcoder/353c.cpp
+++ atcoder/353c.cpp
@@ -16,27 +16,22 @@ void init_code() {
  
 int main(void) {
     init_code();
-    ll n;
+    int n;
     cin >> n;
-
-    vector<ll> A(n);
-
-    REP(i, 0, n) {
-        cin >> A[i];
+    vector<int> a(n);
+    for(auto &v : a) cin >> v;
+    sort(a.begin(), a.end());
+    int r = n;
+    ll cnt = 0, res = 0;
+    for(int i = 0; i < n; i++) {
+        r = max(r, i + 1);
+        while(r - 1 > i and a[r - 1] + a[i] >= 100000000) {
+        r--;
+        }
+        cnt += n - r;
     }
-
-    vector<ll> B(n);
-    B[n-1] = A[n-1];
-    for(int i = n-2; i >= 0; i--) {
-        B[i] += B[i+1] + A[i];
-    }
-
-
-    ll sum = 0;
-    for(ll i = 0; i < n-1; i++) {
-        sum += (B[i+1]%(ll)1e8 + (A[i]%(ll)1e8 * (n-i-1)));
-    }
-
-    cout<<sum<<endl;
+    for(int i = 0; i < n; i++) res += ll(a[i]) * (n - 1);
+    res -= cnt * 100000000;
+    cout << res << endl;
     return 0;
 }
